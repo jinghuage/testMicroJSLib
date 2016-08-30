f936505999fbd26008f4c35750c71f6c6edf8c56
Jinghua Ge <jinghuage@gmail.com>
2016-08-30 14:46:16 -0500

    chore(build): add Travis ci repo, semantic release, and commitizen
    closes #1

---
+++ src/index.test.js
@@ -0,0 +1,28 @@
+var expect = require("chai").expect;
+var snames = require("./index");
+
+describe("test-micro-jslib", function(){
+  describe("all", function(){
+    it("should be an array of strings", function(){
+      expect(snames.all).to.satisfy(isArrayOfStrings);
+
+      function isArrayOfStrings(array){
+        return array.every(function(item){
+          return typeof item === 'string';
+        })
+      }
+    });
+
+    it("should contain mollie", function(){
+      expect(snames.all).to.include("mollie");
+    })
+  });
+
+  describe("random", function(){
+    it("should return a random item from snames.all", function(){
+      var randomItem = snames.random();
+      expect(snames.all).to.include(randomItem);
+    });
+  });
+
+});
